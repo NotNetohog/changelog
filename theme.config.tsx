8f7eb7750f5a219000fdc197f22ae8259cff9278
Neto Hog <neto@bornlogic.com>
2024-03-22 17:19:35 -0300

    first

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,13 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Bornlogic</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/NotNetohog/changelog',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/NotNetohog/changelog',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Bornlogic Changelog',
   },
 }
 
